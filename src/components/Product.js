f6853f026effdf1004f464a2519a512f2dd2feb2
Rio Susanto <riosst100@gmail.com>
2022-10-05 18:32:07 +0700

    Update

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,9 +2,9 @@ import Link from 'next/link';
 import AddToCartButton from '../components/cart/AddToCartButton';
 import Price from "./single-product/price";
 import Image from "../image";
-import {DEFAULT_PRODUCT_HOME_IMG_URL} from "../constants/urls";
+import { DEFAULT_PRODUCT_HOME_IMG_URL } from "../constants/urls";
 
-const Product = ( props ) => {
+const Product = (props) => {
 	const { product } = props;
 
 	return (
@@ -13,14 +13,14 @@ const Product = ( props ) => {
 			<div className="product mb-5">
 
 
-				<Link href={ `/product/${ product?.slug }`} >
+				<Link href={`/product/${product?.slug}`} >
 					<a>
 						<Image
 							className="object-cover bg-gray-100"
 							width="308"
 							height="308"
 							loading="lazy"
-							sourceUrl={ product?.image?.sourceUrl ?? '' }
+							sourceUrl={product?.image?.sourceUrl ?? ''}
 							defaultImgUrl={DEFAULT_PRODUCT_HOME_IMG_URL}
 							altText={product?.image?.altText ?? product?.slug}
 						/>
@@ -28,11 +28,10 @@ const Product = ( props ) => {
 				</Link>
 				<div className="product-info">
 					<h3 className="product-title mt-3 font-medium text-gray-800">
-						{ product.name ? product.name : '' }
+						{product.name ? product.name : ''}
 					</h3>
-					<div className="product-description text-sm text-gray-700" dangerouslySetInnerHTML={{ __html: (product?.description)}}/>
-					<Price salesPrice={product?.price} regularPrice={product?.regularPrice}/>
-					<AddToCartButton product={ product }/>
+					<Price salesPrice={product?.price} regularPrice={product?.regularPrice} />
+					<AddToCartButton product={product} />
 				</div>
 
 			</div>
