8d77f33332cce38b4142452983dbf752f1343a15
Rio Susanto <riosst100@gmail.com>
2022-10-09 20:47:57 +0700

    Styling horizontal categories

--- src/components/category/category-block/ParentCategoriesBlock.js
+++ src/components/category/category-block/ParentCategoriesBlock.js
@@ -64,8 +64,8 @@ const ParentCategoriesBlock = (props) => {
 	return (
 		<>
 			<div className="horizontal-categories">
-				<div class="section-grid flex">
-					<div className="title">Kategori</div>
+				<div className="section-grid flex">
+					<div className="title">KATEGORI</div>
 					<a href="/category" className="action-link">Lihat Lainnya <svg enableBackground="new 0 0 11 11" viewBox="0 0 11 11" role="img" className="icon"><path stroke="none" d="m2.5 11c .1 0 .2 0 .3-.1l6-5c .1-.1.2-.3.2-.4s-.1-.3-.2-.4l-6-5c-.2-.2-.5-.1-.7.1s-.1.5.1.7l5.5 4.6-5.5 4.6c-.2.2-.2.5-.1.7.1.1.3.2.4.2z"></path></svg></a>
 				</div>
 				{productCategories.length ?
