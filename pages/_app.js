c76a0768465004d9ca8466ad5215480de2f68a0d
Rio Susanto <riosst100@gmail.com>
2022-10-10 13:50:45 +0700

    Progress Bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,37 @@
 import "../src/styles/style.scss";
 import "../src/styles/main.scss";
 
-import Router from 'next/router';
-import NProgress from 'nprogress';
-import React from "react";
+import Router, { useRouter } from 'next/router';
+import NProgress from '../src/components/NProgress'
+import React, { useState, useEffect } from "react";
+// import ReactLoading from 'react-loading';
 
 NProgress.configure({ showSpinner: false });
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
+
+let timeout;
+Router.events.on('routeChangeStart', () => {
+  timeout = setTimeout(() => {
+    NProgress.start()
+  }, 100);
+});
+Router.events.on('routeChangeComplete', () => {
+  clearTimeout(timeout);
+  NProgress.done()
+});
+Router.events.on('routeChangeError', () => {
+  clearTimeout(timeout);
+  NProgress.done()
+});
+
+if (typeof window !== 'undefined') {
+  NProgress.start();
+  timeout = setTimeout(() => {
+    NProgress.done();
+  }, 1000);
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
 export default MyApp
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,17 +4,12 @@ import Header from "./Header";
 import Footer from "./Footer";
 import client from "./ApolloClient";
 import Router from "next/router";
-import NProgress from "nprogress";
 import { ApolloProvider } from "@apollo/client";
 import TopHeader from "./TopHeader";
 import Menu from "./Menu";
 import React from "react";
 import BottomMenu from "./BottomMenu";
 import SubMenu from "./SubMenu";
-
-Router.events.on("routeChangeStart", () => NProgress.start());
-Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
 
 const Layout = (props) => {
   return (
