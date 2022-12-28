dccddf03655459bec1ae320b480b0a7ad4951829
riosst <riosst100@gmail.com>
2022-12-28 12:42:36 +0700

    Update

--- pages/category/index.js
+++ pages/category/index.js
@@ -3,17 +3,22 @@ import client from '../../src/components/ApolloClient';
 import ParentCategoriesBlock from "../../src/components/category/category-block/ParentCategoriesBlock";
 import PRODUCTS_AND_CATEGORIES_QUERY from "../../src/queries/product-and-categories";
 import React, { useState } from 'react';
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 export default function CategoryList(props) {
     const { productCategories } = props || {};
+
+    const { t } = useTranslation("");
+
     return (
-        <Layout title="Semua Kategori" menuTitle="Kategori">
-            <ParentCategoriesBlock productCategories={productCategories} />
+        <Layout title="Semua Kategori" menuTitle="Kategori" t={t}>
+            <ParentCategoriesBlock productCategories={productCategories} t={t} />
         </Layout>
     )
 };
 
-export async function getStaticProps() {
+export async function getStaticProps({ locale }) {
 
     const { data } = await client.query({
         query: PRODUCTS_AND_CATEGORIES_QUERY,
@@ -21,6 +26,10 @@ export async function getStaticProps() {
 
     return {
         props: {
+            ...(await serverSideTranslations(locale, [
+				'common',
+				'footer',
+			])),
             productCategories: data?.productCategories?.nodes ? data.productCategories.nodes : []
         },
         revalidate: 1
