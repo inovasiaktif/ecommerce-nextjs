1550cd6c436f84e6a30012a75f2498632643133a
Rio Susanto <riosst100@gmail.com>
2023-04-01 15:27:59 +0700

    Add search icon

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -92,7 +92,20 @@ const HomepageMenu = ({ t, isHomepage }) => {
                     }}/>
                 </Link>
                 <div className="menu-item w-full">
-                    <input name="q" placeholder="Cari jasa atau produk..." className="search-input" type="text" autoComplete="off" />
+                    <form method="GET">
+                        <div className="relative text-gray-600 focus-within:text-gray-400">
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                            <button type="submit" className="p-1 focus:outline-none focus:shadow-outline">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" style={
+                                {
+                                    "width":"20px"
+                                }
+                            }><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                            </button>
+                        </span>
+                        <input type="search" name="q" className="search-input py-2 text-sm text-white pl-10 focus:outline-none focus:bg-white focus:text-gray-900" placeholder="Cari produk atau jasa..." autoComplete="off" />
+                        </div>
+                    </form>
                 </div>
                 <div className="menu-item cart-icon-container">
                     <Link href="/cart">
