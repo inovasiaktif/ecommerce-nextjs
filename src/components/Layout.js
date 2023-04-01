fcb78ccb6352e8a09043e2342d9cc4ebc7c05ca2
Rio Susanto <riosst100@gmail.com>
2023-04-01 19:06:25 +0700

    update

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -16,7 +16,6 @@ const Layout = ({ pageType, title, isHomepage, menuTitle, children, product, noI
           <link rel="shortcut icon" href="/images/logo.png" />
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" />
           <meta charSet="utf-8" />
-          <meta content="width=device-width, initial-scale=1.0" name="viewport" />
           {noIndex && <meta name="robots" content="noindex" />}
         </Head>
         <Menu isHomepage={isHomepage} menuTitle={menuTitle} pageType={pageType} />
